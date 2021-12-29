c7a2a479defb28fc252cf11b1babd9998cda80c6
Carmine Watcher <CWatcher@student.21-school.ru>
2021-12-29 04:26:01 +0300

    [bugfix] Set -> Array

--- Ex1/B. Triangle/Main.java
+++ Ex1/B. Triangle/Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		SortedSet<Integer> sides = new TreeSet<>();
+		int[] sides = new int[3];
 		for (int i = 0; i < 3; i++)
-			sides.add(sc.nextInt());
-		if (sides.headSet(sides.last()).stream().mapToInt(x -> x).sum() > sides.last())
+			sides[i] = sc.nextInt();
+		Arrays.sort(sides);
+		if (sides[0] + sides[1] > sides[2])
 			System.out.println("YES");
 		else
 			System.out.println("NO");
