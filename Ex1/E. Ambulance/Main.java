1154230e8f99b055c11ebb43c7658c6f5f8d4fb9
Carmine Watcher <CWatcher@student.21-school.ru>
2021-12-30 04:28:00 +0300

    [rename] floor -> level

--- Ex1/E. Ambulance/Main.java
+++ Ex1/E. Ambulance/Main.java
@@ -7,44 +7,44 @@ public class Main {
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int A1 = sc.nextInt();
-		int Fs = sc.nextInt();
-		int A2 = sc.nextInt();
-		int S2 = sc.nextInt();
-		int F2 = sc.nextInt();
+		int A1 = sc.nextInt(); // Apartment1
+		int Ls = sc.nextInt(); // Levels per section
+		int A2 = sc.nextInt(); // Apartment2
+		int S2 = sc.nextInt(); // Section2
+		int L2 = sc.nextInt(); // Level2
 
-		int FT2 = (S2 - 1) * Fs + F2;
-		if ( A2 < FT2 || F2 > Fs )
+		int LT2 = (S2 - 1) * Ls + L2; // LevelTotal2
+		if ( A2 < LT2 || L2 > Ls )
 			exit("-1 -1");
-		int AfL = A2 / FT2;
-		if ( A2 % FT2 != 0 )
-			AfL++;
-		if ( FT2 == 1 ) {
-			if (A1 <= A2 || A1 <= AfL * Fs )
+		int AlL = A2 / LT2; // Apartments per level Low
+		if ( A2 % LT2 != 0 )
+			AlL++;
+		if ( LT2 == 1 ) {
+			if (A1 <= A2 || A1 <= AlL * Ls )
 				System.out.print(1 + " ");
 			else
 				System.out.print(0 + " ");
-			if ( A1 <= A2 || Fs == 1 )
+			if ( A1 <= A2 || Ls == 1 )
 				System.out.println(1);
 			else
 				System.out.println(0);
 			return;
 		}
-		int AfH = (A2 - 1) / (FT2 - 1);
-		if ( AfL > AfH )
+		int AlH = (A2 - 1) / (LT2 - 1);  // Apartments per level High
+		if ( AlL > AlH )
 			exit("-1 -1");
-		int AsL = AfL * Fs;
-		int AsH = AfH * Fs;
-		int S1L = (int)Math.ceil( (double)A1 / AsL );
-		int S1H = (int)Math.ceil( (double)A1 / AsH );
+		int AsL = AlL * Ls; // Apartments per section Low
+		int AsH = AlH * Ls; // Apartments per section High
+		int S1L = (int)Math.ceil( (double)A1 / AsL ); // Section1 Low
+		int S1H = (int)Math.ceil( (double)A1 / AsH ); // Section1 High
 		if ( S1L == S1H )
 			System.out.print(S1L + " ");
 		else
 			System.out.print(0 + " ");
-		int F1L = (int)Math.ceil( (double)(A1 - (S1L - 1) * AsL) / AfL );
-		int F1H = (int)Math.ceil( (double)(A1 - (S1H - 1) * AsH) / AfH );
-		if ( F1L == F1H )
-			System.out.println(F1L);
+		int L1L = (int)Math.ceil( (double)(A1 - (S1L - 1) * AsL) / AlL ); // Leve1 Low
+		int L1H = (int)Math.ceil( (double)(A1 - (S1H - 1) * AsH) / AlH ); // Leve1 High
+		if ( L1L == L1H )
+			System.out.println(L1L);
 		else
 			System.out.println(0);
 	}
