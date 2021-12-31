6da84169b28f20206f756fe3b81a7b7d44d6eb10
Carmine Watcher <CWatcher@student.21-school.ru>
2021-12-31 06:55:44 +0300

    + C++ version

---
+++ Ex1/E. Ambulance/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+static void exit_me(string s) {
+	cout << s;
+	exit(0);
+}
+int main() {
+	int A1; // Apartment1
+	int Ls; // Levels per section
+	int A2; // Apartment2
+	int S2; // Section2
+	int L2; // Level2
+	cin >> A1 >> Ls >> A2 >> S2 >> L2;
+	int LT2 = (S2 - 1) * Ls + L2; // LevelTotal2
+	if ( A2 < LT2 || L2 > Ls )
+		exit_me("-1 -1");
+	int AlL = ceil( (double) A2 / LT2 ); // Apartments per level Low
+	if ( LT2 == 1 ) {
+		if (A1 <= A2 || A1 <= AlL * Ls )
+			cout << 1 << " ";
+		else
+			cout << 0 << " ";
+		if ( A1 <= A2 || Ls == 1 )
+			cout << 1 << endl;
+		else
+			cout << 0 << endl;
+		return 0;
+	}
+	int AlH = (A2 - 1) / (LT2 - 1);  // Apartments per level High
+	if ( AlL > AlH )
+		exit_me("-1 -1");
+	int AsL = AlL * Ls; // Apartments per section Low
+	int AsH = AlH * Ls; // Apartments per section High
+	int S1L = ceil( (double)A1 / AsL ); // Section1 Low
+	int S1H = ceil( (double)A1 / AsH ); // Section1 High
+	if ( S1L == S1H )
+		cout << S1L << " ";
+	else
+		cout << 0 << " ";
+	int L1L = ceil( (double)(A1 - (S1L - 1) * AsL) / AlL ); // Leve1 Low
+	int L1H = ceil( (double)(A1 - (S1H - 1) * AsH) / AlH ); // Leve1 High
+	if ( L1L == L1H )
+		cout << L1L << endl;
+	else
+		cout << 0 << endl;
+}
