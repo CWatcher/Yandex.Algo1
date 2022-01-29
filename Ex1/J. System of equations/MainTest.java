9cfe545a9ea7c166db8fb327e50b67f9ee502250
Carmine Watcher <CWatcher@student.21-school.ru>
2022-01-29 23:09:48 +0300

    + case 5, 1

--- Ex1/J. System of equations/MainTest.java
+++ Ex1/J. System of equations/MainTest.java
@@ -10,11 +10,15 @@ class MainTest {
 
 //@org.junit.jupiter.api.Test
 @Test
-void main() {
-	main("1 1 0    1 1 1", "0");
+void test() {
+	test("1 1 0    1 1 1", "0");
+	test("0 0 0    0 0 1", "0");
+	test("0 0 0    0 0 0", "5");
+	test("1 2 1    2 4 2", "1 -0.5 0.5");
+	test("0 0 0   -2 1 3", "1 2.0 3.0");
 }
 
-void main( String input, String output ) {
+void test( String input, String output ) {
 	System.setIn( new ByteArrayInputStream( input.getBytes() ) );
 	ByteArrayOutputStream out = new ByteArrayOutputStream();
 	System.setOut( new PrintStream( out ) );
